123ad811453ced74dd6bb0fe35fbaeb2bda9b571
Russell Anderson <5637107+rpanderson@users.noreply.github.com>
2020-04-27 02:34:56 +1000

    Beginnings of sphinx documentation.

---
+++ docs/source/api.rst
@@ -0,0 +1,10 @@
+API Reference
+=============
+
+Functions, classes, and methods for use in lyse analysis routines.
+
+.. toctree::
+   :maxdepth: 1
+   :glob:
+
+   ../api/*
---
+++ docs/source/conf.py
@@ -0,0 +1,84 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+import sphinx_rtd_theme
+from lyse import __version__
+sys.path.insert(0, os.path.abspath('../..'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'lyse'
+copyright = '2020, labscript suite'
+author = 'labscript suite'
+
+version = __version__
+release = version
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx_rtd_theme",
+    "recommonmark",
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# The suffix(es) of source filenames.
+source_suffix = ['.rst', '.md']
+
+# Custom parsers of source files.
+# source_parsers = {'.md': 'recommonmark.parser.CommonMarkParser'}
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+# intersphinx allows us to link directly to other repos sphinxdocs.
+# https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html
+# intersphinx_mapping = {
+    # 'python': ('https://docs.python.org/3', None),
+    # 'numpy': ('https://numpy.org/doc/stable/', None),
+    # 'scipy': ('https://docs.scipy.org/doc/scipy/reference/', None),
+    # 'pandas': ('https://pandas.pydata.org/pandas-docs/stable/', None)
+# }
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+# html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+html_logo = "img/lyse.png"
+html_favicon = "img/lyse.ico"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = []
+
+# The name of the Pygments (syntax highlighting) style to use.
+# pygments_style = "sphinx"
